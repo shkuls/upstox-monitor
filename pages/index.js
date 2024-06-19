134d03f75b20cc8d2da19b3478a3a5d4e959e3cf
shkuls <diy2903@gmail.com>
2024-06-20 04:12:14 +0530

    revised structure

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import Home from './isLoggedin/Home'
 import Login from './Login/Login'
 import axios from 'axios'
-export const tokenContext = createContext("");
 
 export default function index() {
   
   const [isLoggedin , setLogin] = useState(false)
-  const [accessToken , setAccessToken] = useState("");
       useEffect(()=>{
         
-        axios.get("http://localhost:8000/getAccess")
+        axios.get("http://localhost:8000/getLogin")
         .then((response)=>{
           console.log(response.data)
           if(response.data)
@@ -22,11 +20,10 @@ export default function index() {
 
       })
   return (
-<tokenContext.Provider value={accessToken}>
 
     <div  className='h-5/6'>
       { isLoggedin ?<Home/> : <Login/>}
     </div>
-</tokenContext.Provider>
+
   )
 }
--- pages/isLoggedin/positions/Positions.js
+++ pages/isLoggedin/positions/Positions.js
@@ -1,11 +1,9 @@
-import React , {useContext} from 'react'
+import React  from 'react'
 import HoldingCard from '../../../components/HoldingCard'
-import { tokenContext } from '../..'
-// const response = [1,2]
+
 
 export default function Positions() {
-  const {token} = useContext(tokenContext)
-  console.log(token)
+  
   return (
     <div>
      {/* {response.data.map((stock)=>
